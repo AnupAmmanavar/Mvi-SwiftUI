c586e72df87e8456eb1d900e910b144c87991fd0
AnupPorter <anup.a@theporter.in>
2020-04-13 17:56:11 +0530

    Initial Commit

---
+++ MovieSearch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieSearch
+//
+//  Created by porter on 13/04/20.
+//  Copyright © 2020 kinley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
